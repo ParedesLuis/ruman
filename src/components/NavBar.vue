b45f7865fe085cb3e7b4727e87d56ef3065df6b2
Alberto156 <lm5708144@gmail.com>
2021-01-28 02:18:09 -0400

    functionality add

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,13 +13,14 @@
         <v-divider class="sm-2" vertical></v-divider>
 
         <v-toolbar-title class="hidden-sm-and-down">Page Title</v-toolbar-title>
+
         <v-app-bar-nav-icon
           class="hidden-md-and-up"
           @click.stop="drawer = !drawer"
         ></v-app-bar-nav-icon>
 
+        <!--List Element-->
         <v-spacer></v-spacer>
-
         <v-card class="hidden-sm-and-down" centered color="black">
           <v-tabs dark background-color="black">
             <v-tab>Guie</v-tab>
@@ -29,12 +30,44 @@
           </v-tabs>
         </v-card>
 
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+          color="black"
+          height="100vh"
+        >
+          <v-list nav dense>
+            <v-list-item-group
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item>
+                <v-list-item-title>Guie</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Documents</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Components</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Licenses</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+
+        <!--Login Modal-->
         <v-spacer></v-spacer>
         <Dialog />
-        <v-divider vertical></v-divider>
       </v-app-bar>
     </div>
 
+    <!--Container Table-->
     <div class="center">
       <v-simple-table>
         <template v-slot:default>
