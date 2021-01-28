f48b630d555059c6bff413a1e5ee0a42f9aabc5b
Luis Paredes <32340615+ParedesLuis@users.noreply.github.com>
2021-01-28 15:09:55 -0400

    Styles

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -2,58 +2,26 @@
   <v-row justify="center">
     <v-dialog
       v-model="dialog"
-      persistent
       max-width="600px"
+      align-center
     >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
           color="primary"
-          dark
           v-bind="attrs"
           v-on="on"
         >
           Login
         </v-btn>
       </template>
-      <v-card>
+      <v-card dark class="card">
         <v-card-title>
-          <span class="headline">User Profile</span>
+          <span class="headline">Login</span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-text-field
-                  label="Legal first name*"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-text-field
-                  label="Legal middle name"
-                  hint="example of helper text only on focus"
-                ></v-text-field>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
+              
               <v-col cols="12">
                 <v-text-field
                   label="Email*"
@@ -67,46 +35,20 @@
                   required
                 ></v-text-field>
               </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
+              
+              <v-btn
+                    color="primary"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Enter
+                  </v-btn>
+
             </v-row>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -121,3 +63,9 @@ export default {
   }),
 };
 </script>
+
+<style  scoped>
+.card {
+  color: white
+}
+</style>
